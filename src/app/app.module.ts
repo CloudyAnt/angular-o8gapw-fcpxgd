bcb0aa6b371d0de21696b0d2196aa21c54493de9
CloudyAnt <itscloudy@foxmail.com>
2019-10-23 19:02:19 +0800

    add product-details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { FsdfComponent } from './fsdf/fsdf.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
@@ -15,6 +15,7 @@ import { FsdfComponent } from './fsdf/fsdf.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent}
     ])
   ],
   declarations: [
@@ -22,7 +23,7 @@ import { FsdfComponent } from './fsdf/fsdf.component';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    FsdfComponent
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
